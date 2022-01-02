12d49f6edef854cccd52cea56493a252d003b323
BVBFD <lsevina126@gmail.com>
2022-01-02 23:13:53 +0900

    Code service part

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ menu.onclick = () => {
   header.classList.toggle("active");
   document.body.classList.toggle("active");
 };
+
+let swiper = new Swiper(".products-slider", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidePerView: 1,
+    },
+    768: {
+      slidePerView: 2,
+    },
+    991: {
+      slidePerView: 3,
+    },
+  },
+});
